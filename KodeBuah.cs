c5c13efd72fcd226bafff6edd187823d06568fcb
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-03-13 21:13:49 +0800

    feat: menambahkan class KodeBuah

---
+++ KodeBuah.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul4_103082400005
+{
+    internal class KodeBuah
+    {
+
+        public enum NamaBuah {
+            Apel,       //0
+            Aprikot,    //1
+            Alpukat,    //2
+            Pisang,     //3
+            Paprika,    //4
+            Blackberry, //5
+            Ceri,       //6
+            Kelapa,     //7
+            Jagung,     //8
+            Kurma,      //9
+            Durian,     //10
+            Anggur,     //11
+            Melon,      //12
+            Semangka    //13
+        }
+
+        private static readonly String[] kodeBuah =
+            [
+            "A00", //0
+            "B00", //1
+            "C00", //2
+            "D00", //3
+            "E00", //4
+            "F00", //5
+            "H00", //6
+            "I00", //7
+            "J00", //8
+            "K00", //9
+            "L00", //10
+            "M00", //11
+            "N00", //12
+            "O00"  //13
+            ];
+
+
+        public static String GetKodeBuah(NamaBuah buah)
+        {
+            return kodeBuah[(int)buah];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul4_103082400005;
+
+public class Program{
+
+    public static void Main(String[] args)
+    {
+        KodeBuah.NamaBuah searchBuah = KodeBuah.NamaBuah.Apel;
+
+        Console.WriteLine(KodeBuah.GetKodeBuah(searchBuah));
+
+    }
+
+}
